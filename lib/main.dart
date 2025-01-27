64f904ef6e7e49aab8cd07c37735ae063f4fd4a2
Hugo Pierre <hugo.pierre12255@gmail.com>
2025-01-27 16:59:54 -0300

    Alinhar os tubos e telinha de menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
@@ -11,6 +12,9 @@ class MyGame extends FlameGame with TapDetector {
   double gravity = 300;
   double velocityY = 0;
   double groundLevel = 780;
+  double objectSpeed = 200;
+  Random random = Random();
+
   late SpriteComponent background;
   late SpriteComponent player;
   late SpriteComponent object1;
@@ -18,6 +22,7 @@ class MyGame extends FlameGame with TapDetector {
 
   @override
   Future<void> onLoad() async {
+
     background = SpriteComponent()
       ..sprite = await Sprite.load('Lukewarm_Ocean.webp')
       ..size = size
@@ -35,7 +40,7 @@ class MyGame extends FlameGame with TapDetector {
 
     object2 = SpriteComponent()
       ..sprite = await Sprite.load('pilar.png')
-      ..position = Vector2(500, -500)
+      ..position = Vector2(800, -500)
       ..size = Vector2(60, 700);
 
     add(background);
@@ -44,22 +49,35 @@ class MyGame extends FlameGame with TapDetector {
     add(object2);
   }
 
+  @override
   void update(double dt) {
     super.update(dt);
 
-
     velocityY += gravity * dt;
-
-
     player.position.y += velocityY * dt;
 
     if (player.position.y >= groundLevel) {
       player.position.y = groundLevel;
       velocityY = 0;
     }
+
+    object1.position.x -= objectSpeed * dt;
+
+    if (object1.position.x + object1.size.x < 0) {
+      object1.position.x = size.x;
+      object1.position.y = random.nextDouble() * (groundLevel - 200);
+    }
+
+    object2.position.x -= objectSpeed * dt;
+
+    if (object2.position.x + object2.size.x < 0) {
+      object2.position.x = size.x + 300;
+      object2.position.y = random.nextDouble() * (-200);
+    }
   }
+
+  @override
   void onTap() {
     velocityY = -200;
   }
 }
-
