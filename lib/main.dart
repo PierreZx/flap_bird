0770f66293483bda01f4492e735285429022f63d
Hugo Pierre <hugo.pierre12255@gmail.com>
2025-01-27 16:13:40 -0300

    Peixe Pulando

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/widgets.dart';
@@ -6,7 +7,10 @@ void main() {
   runApp(GameWidget(game: MyGame()));
 }
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with TapDetector {
+  double gravity = 300;
+  double velocityY = 0;
+  double groundLevel = 780;
   late SpriteComponent background;
   late SpriteComponent player;
   late SpriteComponent object1;
@@ -14,31 +18,48 @@ class MyGame extends FlameGame {
 
   @override
   Future<void> onLoad() async {
-    // Carrega e adiciona o fundo
     background = SpriteComponent()
-      ..sprite = await Sprite.load('teste.webp')
+      ..sprite = await Sprite.load('Lukewarm_Ocean.webp')
       ..size = size
       ..position = Vector2.zero();
 
-    // Carrega e adiciona o jogador
     player = SpriteComponent()
-      ..sprite = await Sprite.load('player.webp')
-      ..position = Vector2(100, 100)
-      ..size = Vector2(50, 50);
+      ..sprite = await Sprite.load('player.png')
+      ..position = Vector2(100, 220)
+      ..size = Vector2(100, 100);
 
     object1 = SpriteComponent()
-      ..sprite = await Sprite.load('pilar.jpg')
+      ..sprite = await Sprite.load('pilar.png')
       ..position = Vector2(500, 400)
       ..size = Vector2(60, 700);
 
     object2 = SpriteComponent()
-      ..sprite = await Sprite.load('pilar.jpg')
-      ..position = Vector2(500, 0)
-      ..size = Vector2(60, 100);
+      ..sprite = await Sprite.load('pilar.png')
+      ..position = Vector2(500, -500)
+      ..size = Vector2(60, 700);
 
     add(background);
     add(player);
     add(object1);
     add(object2);
   }
+
+  void update(double dt) {
+    super.update(dt);
+
+
+    velocityY += gravity * dt;
+
+
+    player.position.y += velocityY * dt;
+
+    if (player.position.y >= groundLevel) {
+      player.position.y = groundLevel;
+      velocityY = 0;
+    }
+  }
+  void onTap() {
+    velocityY = -200;
+  }
 }
+
